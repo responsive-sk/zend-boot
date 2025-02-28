87d99e58f858c5eec94e872a12f19f42732a0565
alexmerlin <alex.merlin.1985@gmail.com>
2025-02-28 12:46:57 +0200

    Issue #44: Added full config for dot-errorhandler
    Signed-off-by: alexmerlin <alex.merlin.1985@gmail.com>

--- config/autoload/error-handling.global.php
+++ config/autoload/error-handling.global.php
@@ -3,8 +3,20 @@
 declare(strict_types=1);
 
 use Dot\ErrorHandler\Extra\ExtraProvider;
+use Dot\ErrorHandler\Extra\Processor\CookieProcessor;
+use Dot\ErrorHandler\Extra\Processor\HeaderProcessor;
+use Dot\ErrorHandler\Extra\Processor\ProcessorInterface;
+use Dot\ErrorHandler\Extra\Processor\RequestProcessor;
+use Dot\ErrorHandler\Extra\Processor\ServerProcessor;
+use Dot\ErrorHandler\Extra\Processor\SessionProcessor;
 use Dot\ErrorHandler\Extra\Processor\TraceProcessor;
+use Dot\ErrorHandler\Extra\Provider\CookieProvider;
+use Dot\ErrorHandler\Extra\Provider\HeaderProvider;
+use Dot\ErrorHandler\Extra\Provider\RequestProvider;
+use Dot\ErrorHandler\Extra\Provider\ServerProvider;
+use Dot\ErrorHandler\Extra\Provider\SessionProvider;
 use Dot\ErrorHandler\Extra\Provider\TraceProvider;
+use Dot\ErrorHandler\Extra\ReplacementStrategy;
 use Dot\Log\Formatter\Json;
 use Dot\Log\Logger;
 
@@ -13,7 +25,50 @@ return [
         'loggerEnabled'           => true,
         'logger'                  => 'dot-log.default_logger',
         ExtraProvider::CONFIG_KEY => [
-            TraceProvider::class => [
+            CookieProvider::class  => [
+                'enabled'   => false,
+                'processor' => [
+                    'class'               => CookieProcessor::class,
+                    'replacementStrategy' => ReplacementStrategy::Full,
+                    'sensitiveParameters' => [
+                        ProcessorInterface::ALL,
+                    ],
+                ],
+            ],
+            HeaderProvider::class  => [
+                'enabled'   => false,
+                'processor' => [
+                    'class'               => HeaderProcessor::class,
+                    'replacementStrategy' => ReplacementStrategy::Full,
+                ],
+            ],
+            RequestProvider::class => [
+                'enabled'   => false,
+                'processor' => [
+                    'class'               => RequestProcessor::class,
+                    'replacementStrategy' => ReplacementStrategy::Full,
+                    'sensitiveParameters' => [
+                        'password',
+                    ],
+                ],
+            ],
+            ServerProvider::class  => [
+                'enabled'   => false,
+                'processor' => [
+                    'class'               => ServerProcessor::class,
+                    'replacementStrategy' => ReplacementStrategy::Full,
+                    'sensitiveParameters' => [
+                        ProcessorInterface::ALL,
+                    ],
+                ],
+            ],
+            SessionProvider::class => [
+                'enabled'   => false,
+                'processor' => [
+                    'class' => SessionProcessor::class,
+                ],
+            ],
+            TraceProvider::class   => [
                 'enabled'   => true,
                 'processor' => [
                     'class' => TraceProcessor::class,
